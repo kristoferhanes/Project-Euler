6d179af70a46383836a7dd220998a6d6c67917d2
Kristofer Hanes <kristoferhanes@Mac-mini.local>
2016-04-07 12:56:25 -0700

    Optimizations

--- Problem-3.playground/Contents.swift
+++ Problem-3.playground/Contents.swift
@@ -1,6 +1,31 @@
 
-let largestPrimeFactor = factors(600851475143)
-  .filter(isPrime)
-  .maxElement()
+import Foundation
 
-largestPrimeFactor
+func largestPrimeFactor(of n: Int) -> Int {
+
+  func sqrt(n: Int) -> Int {
+    return Int(Foundation.sqrt(Double(n)))
+  }
+
+  func isPrime(n: Int) -> Bool {
+    for divisor in 2...sqrt(n) {
+      if n % divisor == 0 {
+        return false
+      }
+    }
+    return true
+  }
+
+  var result = 1
+
+  for divisor in 2...sqrt(n) {
+    if n % divisor == 0 && isPrime(divisor) {
+      result = divisor
+    }
+  }
+
+  return result
+}
+
+largestPrimeFactor(of: 13195) == 29
+largestPrimeFactor(of: 600851475143)
