2d3d052128f03537f796c7d286044de669a70599
Kristofer Hanes <kristoferhanes@Mac-mini.local>
2016-04-08 13:33:03 -0700

    Optimizations

--- Problem-4.playground/Contents.swift
+++ Problem-4.playground/Contents.swift
@@ -1,4 +1,28 @@
 
-import UIKit
+import Foundation
 
-let result = largestPalindrome(from: 100, to: 999)
+func largestPalindromeProduct(from from: Int, to: Int) -> Int? {
+
+  func isPalindrome(n: Int) -> Bool {
+    let s = String(n)
+    let r = String(s.characters.reverse())
+    return s == r
+  }
+
+  var result: Int?
+  for i in from...to {
+    for j in i...to {
+      let product = i * j
+      guard product > result else { continue }
+      if isPalindrome(product) {
+        result = product
+      }
+    }
+  }
+  return result
+}
+
+
+largestPalindromeProduct(from: 10, to: 99) == 9009
+let result = largestPalindromeProduct(from: 100, to: 999)
+result
