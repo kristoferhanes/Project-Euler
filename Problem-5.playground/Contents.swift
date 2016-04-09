85dc9fbcac7de9d783b042f073344b278165d25e
Kristofer Hanes <kristoferhanes@gmail.com>
2016-04-09 12:58:16 -0700

    Optimizations.

--- Problem-5.playground/Contents.swift
+++ Problem-5.playground/Contents.swift
@@ -1,5 +1,16 @@
 
-import UIKit
 
+func smallestNumberDivisibleByAll(xs: [Int]) -> Int? {
+  guard let largest = xs.maxElement() else { return nil }
+  outer: for n in (largest*2).stride(to: Int.max, by: largest) {
+    for x in xs {
+      if n % x != 0 { continue outer }
+    }
+    return n
+  }
+  return nil
+}
+
+smallestNumberDivisibleByAll([Int](1...10)) == 2520
 let result = smallestNumberDivisibleByAll([Int](1...20))
 result
--- Problem-5.playground/Sources/SupportCode.swift
+++ Problem-5.playground/Sources/SupportCode.swift
@@ -1,10 +1,2 @@
 import Foundation
 
-public func smallestNumberDivisibleByAll(xs: [Int]) -> Int? {
-  if xs.isEmpty { return nil }
-  let largest = xs.sort(>).first!
-  for n in (largest*2).stride(to: Int.max, by: largest) {
-    if (xs.filter { n % $0 != 0 }).isEmpty  { return n }
-  }
-  return nil
-}
