6e6943973d929b3fbd6f0ae990661b25db65b51f
Kristofer Hanes <kristoferhanes@Mac-mini.local>
2016-04-11 14:50:04 -0700

    Optimizations.

--- Problem-9.playground/Contents.swift
+++ Problem-9.playground/Contents.swift
@@ -1,27 +1,18 @@
 
-import UIKit
 
-func isPyth(a: Int, _ b: Int, _ c: Int) -> Bool {
-  return a*a + b*b == c*c
-}
-
-func pythWithSum(sum: Int) -> (Int,Int,Int)? {
-  for a in 1...(sum/3-1) {
-    for b in (a+1)...(sum*2/3-1) {
-      let c = sum-(a+b)
-      if isPyth(a, b, c) {
-        return (a, b, c)
-      }
-    }
+func productOfPythagoreanTriple(withSum sum: Int) -> Int? {
+  for a in 1..<sum {
+    let x = Double(sum * (sum - 2*a)) / Double(2 * (sum - a))
+    guard x == Double(Int(x)) else { continue }
+    let b = Int(x)
+    let c = sum - (a + b)
+    return a * b * c
   }
   return nil
 }
 
-func productOfPythWithSum(sum: Int) -> Int? {
-  return pythWithSum(sum).map { $0 * $1 * $2 }
-}
 
+productOfPythagoreanTriple(withSum: 3+4+5)! == 3*4*5
 
-productOfPythWithSum(3+4+5) == 3*4*5
-
-let solution = productOfPythWithSum(1000)
+let solution = productOfPythagoreanTriple(withSum: 1000)
+solution
