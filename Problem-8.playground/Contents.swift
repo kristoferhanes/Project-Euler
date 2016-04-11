265db16dd6230a99d596d8533861c34b305cea0c
Kristofer Hanes <kristoferhanes@Mac-mini.local>
2016-04-11 11:33:51 -0700

    Optimizations.

--- Problem-8.playground/Contents.swift
+++ Problem-8.playground/Contents.swift
@@ -1,45 +1,20 @@
 
-import UIKit
-
 let digits = "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450"
   .characters.map { Int(String($0))! }
 
-extension Array {
-  func split(length: Int) -> [ArraySlice<Element>] {
-    var result: [ArraySlice<Element>] = []
-    for i in startIndex..<endIndex-length {
-      result.append(self[i..<i+length])
-    }
-    return result
-  }
-}
 
-func adjDigits(n: Int, _ xs: [Int]) -> [ArraySlice<Int>] {
-  return xs.split(n)
-}
-
-func adjDigitsProducts(xss: [ArraySlice<Int>]) -> [(Int,ArraySlice<Int>)] {
-  typealias Element = (Int,ArraySlice<Int>)
-  var result: [Element] = []
-  for xs in xss {
-    let product = xs.reduce(1, combine: *)
-    let x = (product, xs)
-    result.append(x)
+func adjDigitsMaxProduct(xs: [Int], length: Int) -> Int? {
+  var result: Int?
+  for i in xs.startIndex..<xs.endIndex-length {
+    let product = xs[i..<i+length].reduce(1, combine: *)
+    guard product > result else { continue }
+    result = product
   }
   return result
 }
 
-func maxAdjDigitsProducts(xs: [(Int,ArraySlice<Int>)]) -> (Int,ArraySlice<Int>)? {
-  return xs.reduce(nil) { max, x in x.0 > max?.0 ? x : max }
-}
 
-func solution(n: Int, _ xs: [Int]) -> (Int,ArraySlice<Int>)? {
-  return adjDigitsProducts(adjDigits(n, xs)).maxElement{$0.0<$1.0}
-}
+adjDigitsMaxProduct(digits, length: 4) == 5832
 
-let test = solution(4, digits)
-test?.0 == 5832
-
-
-let sol = solution(13, digits)
-sol
+let solution = adjDigitsMaxProduct(digits, length: 13)
+solution
