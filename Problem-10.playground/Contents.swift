3f8b89ba312b9dd1599921fb0c1cdf1c79b91e85
Kristofer Hanes <kristoferhanes@Mac-mini.local>
2016-04-11 18:45:19 -0700

    Simplified code.

--- Problem-10.playground/Contents.swift
+++ Problem-10.playground/Contents.swift
@@ -1,9 +1,6 @@
 
-import UIKit
 
-primesBelow(10) == [2,3,5,7]
-sum(primesBelow(10)) == 17
+sumOfPrimes(below: 10) == 17
 
-let solution = sum(primesBelow(2_000_000))
+let solution = sumOfPrimes(below: 2_000_000)
 solution
-
--- Problem-10.playground/Sources/SupportCode.swift
+++ Problem-10.playground/Sources/SupportCode.swift
@@ -1,17 +1,11 @@
 
 
-public func primesBelow(n: Int) -> [Int] {
+public func sumOfPrimes(below n: Int) -> Int {
   var ps = [Int](2..<n)
-  for (i, p) in ps.enumerate() {
-    if p == 0 { continue }
+  for (i, p) in ps.enumerate() where p != 0 {
     for j in (i+p).stride(to: ps.endIndex, by: p) {
       ps[j] = 0
     }
   }
-  return ps.filter { $0 != 0 }
+  return ps.reduce(0, combine: +)
 }
-
-public func sum(xs: [Int]) -> Int {
-  return xs.reduce(0, combine: +)
-}
-
