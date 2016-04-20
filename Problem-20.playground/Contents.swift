5f27f7e2c560899a9882545e2b460b0add5b309b
Kristofer Hanes <kristoferhanes@Mac-mini.local>
2016-04-19 17:16:28 -0700

    Optimizations

--- Problem-20.playground/Contents.swift
+++ Problem-20.playground/Contents.swift
@@ -1,11 +1,10 @@
 
-import UIKit
-
 struct LongInt {
-  let digits: [Int]
+  var digits: [Int]
 }
 
 extension LongInt {
+
   init(_ value: Int) {
     digits = LongInt.split(value)
   }
@@ -21,51 +20,38 @@ extension LongInt {
   }
 
   func times(n: Int) -> LongInt {
-    var result: [Int] = []
+    var copy = self
+    copy.timesInPlace(n)
+    return copy
+  }
+
+  mutating func timesInPlace(n: Int) {
     var carry = 0
-    for x in digits {
-      let product = x * n + carry
-      let digit = product % 10
+    for i in digits.indices {
+      let product = digits[i] * n + carry
+      digits[i] = product % 10
       carry = product / 10
-      result.append(digit)
     }
     while carry > 0 {
-      result.append(carry % 10)
+      digits.append(carry % 10)
       carry /= 10
     }
-    return LongInt(digits: result)
   }
-}
 
-extension LongInt: CustomStringConvertible {
-  var description: String {
-    let ds = digits.reverse()
-    var result: [Character] = []
-    for d in ds {
-      result.append(Character(String(d)))
-    }
-    return String(result)
-  }
 }
 
 func factorial(n: Int) -> LongInt {
-  var result = LongInt(1)
-  for x in 2...n {
-    result = result.times(x)
+  var result = LongInt(2)
+  for x in 3...n {
+    result.timesInPlace(x)
   }
   return result
 }
 
 LongInt.split(12345).reverse() == [1,2,3,4,5]
 LongInt(999).times(2).digits.reverse() == [1,9,9,8]
-LongInt(1234567890).description == "1234567890"
-factorial(10).description == "3628800"
 factorial(10).digits.reduce(0, combine: +) == 27
 
 
 let solution = factorial(100).digits.reduce(0, combine: +)
-print(solution)
-
-
-
-
+solution
