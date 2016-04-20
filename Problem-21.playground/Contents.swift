fe3785212af6743d92802e10cd5a133702688a8b
Kristofer Hanes <kristoferhanes@gmail.com>
2016-04-20 14:14:38 -0700

    formatting

--- Problem-21.playground/Contents.swift
+++ Problem-21.playground/Contents.swift
@@ -1,11 +1,8 @@
-
-import UIKit
-
-[Int](220.properDivisors).reduce(0, combine: +) == 284
-[Int](284.properDivisors).reduce(0, combine: +) == 220
+220.properDivisors.reduce(0, combine: +) == 284
+284.properDivisors.reduce(0, combine: +) == 220
 areAmicable(220, 284)
 
-let solution = amicableNumbersInRange(1..<10000)
-  .reduce(0, combine: +)
-print(solution)
+amicableNumbers(inRange: 1...1000)
 
+let solution = amicableNumbers(inRange: 1..<10000).reduce(0, combine: +)
+solution
--- Problem-21.playground/Sources/SupportCode.swift
+++ Problem-21.playground/Sources/SupportCode.swift
@@ -1,48 +1,41 @@
-
 import Foundation
 
 extension Int {
   public var properDivisors: Set<Int> {
     struct Memo { static var divs = [Int:Set<Int>]() }
-
     if let ds = Memo.divs[self] { return ds }
+
     let lowDivs = lowDivisors
-    let highDivs = highDivisors(lowDivs).subtract([self])
-    let result = lowDivs.union(highDivs)
+    let highDivs = highDivisors(lowDivs)
+    let result = Set(lowDivs).union(highDivs).subtract([self])
+
     Memo.divs[self] = result
     return result
   }
 
-  var lowDivisors: Set<Int> {
+  var lowDivisors: [Int] {
     var sieve = [Int](0...Int(sqrt(Double(self))))
-    for x in sieve {
-      if x == 0 { continue }
-      if self % x != 0 {
-        for i in x.stride(to: sieve.endIndex, by: x) {
-          sieve[i] = 0
-        }
+    for x in sieve where x != 0 && self % x != 0 {
+      for i in x.stride(to: sieve.endIndex, by: x) {
+        sieve[i] = 0
       }
     }
-    return Set(sieve.filter { $0 != 0 })
+    return sieve.filter { $0 != 0 }
   }
 
-  func highDivisors(lds: Set<Int>) -> Set<Int> {
-    var result = Set<Int>()
-    for x in lds {
-      result.insert(self / x)
-    }
-    return result
+  func highDivisors(lds: [Int]) -> [Int] {
+    return lds.map { self / $0 }
   }
 }
 
 public func areAmicable(a: Int, _ b: Int) -> Bool {
-  let sumA = [Int](a.properDivisors).reduce(0, combine: +)
-  let sumB = [Int](b.properDivisors).reduce(0, combine: +)
+  let sumA = a.properDivisors.reduce(0, combine: +)
+  let sumB = b.properDivisors.reduce(0, combine: +)
   return sumA == b && sumB == a
 }
 
-public func amicableNumbersInRange(range: Range<Int>) -> [Int] {
-  var result = [Int]()
+public func amicableNumbers(inRange range: Range<Int>) -> [Int] {
+  var result: [Int] = []
   for i in range.startIndex..<range.endIndex-1 {
     for j in i+1..<range.endIndex {
       if areAmicable(i, j) { result += [i,j] }
@@ -50,4 +43,3 @@ public func amicableNumbersInRange(range: Range<Int>) -> [Int] {
   }
   return result
 }
-
