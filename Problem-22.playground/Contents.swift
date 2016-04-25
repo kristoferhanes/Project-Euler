d669dbf72577f4b129500c3572b120a33881867c
Kristofer Hanes <kristoferhanes@Mac-mini.local>
2016-04-25 16:15:58 -0700

    formatting

--- Problem-22.playground/Contents.swift
+++ Problem-22.playground/Contents.swift
@@ -1,43 +1,41 @@
+import Foundation
 
-import UIKit
+func names(from file: NSURL) -> [String] {
 
-let path = "/Users/kristoferhanes/Dropbox/Developer/Project Euler/Problem-22.playground/Resources/names.txt"
-
-func removeQuotes(s: String) -> String {
-  if s.characters.count < 2 { return s }
-  if s.characters.first != "\"" || s.characters.last != "\"" { return s }
-  let range = s.startIndex.advancedBy(1)..<s.startIndex.advancedBy(s.characters.count-1)
-  return s.substringWithRange(range)
-}
-
-func namesFromFile(path: String) -> [String] {
-  return (try? String(contentsOfFile: path))?.characters.split(",").map { removeQuotes(String($0)) } ?? []
-}
-
-func charValue(c: Character) -> Int {
-  struct Constants { static let alphaOffset = 64 }
-  struct Memo { static var charValues = [Character:Int]() }
-
-  if let cv = Memo.charValues[c] { return cv }
-  let scalars = String(c).unicodeScalars
-  let result = Int(scalars[scalars.startIndex].value) - Constants.alphaOffset
-  Memo.charValues[c] = result
-  return result
+  func removeQuotes(from s: String) -> String {
+    guard s.characters.count >= 2 else { return s }
+    guard s.characters.first == "\"" && s.characters.last == "\"" else { return s }
+    let range = s.startIndex.advancedBy(1)..<s.startIndex.advancedBy(s.characters.count-1)
+    return s.substringWithRange(range)
+  }
+  
+  guard let contents = try? String(contentsOfURL: file) else { return [] }
+  return contents.characters.split(",").map(String.init).map(removeQuotes)
 }
 
 func alphaValue(s: String) -> Int {
+
+  func charValue(c: Character) -> Int {
+    struct Constant { static let AlphaOffset = 64 }
+    struct Memo { static var charValues = [Character:Int]() }
+    if let cv = Memo.charValues[c] { return cv }
+
+    let result = Int(String(c).unicodeScalars.first!.value) - Constant.AlphaOffset
+
+    Memo.charValues[c] = result
+    return result
+  }
+
   return s.uppercaseString.characters.map(charValue).reduce(0, combine: +)
 }
 
-removeQuotes("\"hello\"") == "hello"
 alphaValue("colin") == 53
 
-let solution = namesFromFile(path).sort(<).enumerate()
-  .map { ($0.index+1, alphaValue($0.element)) }
-  .map { $0.0 * $0.1 }
+let file = [#FileReference(fileReferenceLiteral: "names.txt")#]
+
+let solution = names(from: file).sort().enumerate()
+  .map { ($0.index+1) * alphaValue($0.element) }
   .reduce(0, combine: +)
 
 
-print(solution)
-
-
+solution
