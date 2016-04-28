3a254d63b187f8295e132ab09e502f0a043d77ec
Kristofer Hanes <kristoferhanes@Mac-mini.local>
2016-04-27 19:19:18 -0700

    optimizations

--- Problem-5.playground/Contents.swift
+++ Problem-5.playground/Contents.swift
@@ -1,16 +1,18 @@
-
-
-func smallestNumberDivisibleByAll(xs: [Int]) -> Int? {
-  guard let largest = xs.maxElement() else { return nil }
-  outer: for n in (largest*2).stride(to: Int.max, by: largest) {
-    for x in xs {
-      if n % x != 0 { continue outer }
-    }
-    return n
-  }
-  return nil
+func gcd(a: Int, _ b: Int) -> Int {
+  guard a <= b else { return gcd(b, a) }
+  let mod = b % a
+  guard mod != 0 else { return a }
+  return gcd(mod, a)
 }
 
-smallestNumberDivisibleByAll([Int](1...10)) == 2520
-let result = smallestNumberDivisibleByAll([Int](1...20))
-result
+func lcm(a: Int, _ b: Int) -> Int {
+  return a / gcd(a, b) * b
+}
+
+func lcm(range: Range<Int>) -> Int {
+  return range.reduce(1, combine: lcm)
+}
+
+lcm(1...10) == 2520
+let solution = lcm(1...20)
+solution
